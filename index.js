5353dd64e97c913f1ba8e5e66b280a04cb2c48ed
ajohn2550 <alex@johnsonwebware.com>
2016-01-23 15:47:07 -0700

    Starting config

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+global.lib    = __dirname + '/lib';
+global.root   = __dirname;
+global.config = require('./config')[process.env.NODE_ENV || 'development'];
+global.util   = require('util');
+
+require(global.lib + '/init')
+	.set('view engine', 'jade')
+;

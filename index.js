248544ce82391bcc534bc0a458640036c9218dcc
ajohn2550 <alex@johnsonwebware.com>
2016-01-24 22:14:08 -0700

    Github auth using user perms

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ global.root   = __dirname;
 global.config = require('./config')[process.env.NODE_ENV || 'development'];
 global.util   = require('util');
 
+
 require(global.lib + '/init')
 	.set('view engine', 'jade')
 	.locals.pretty = true;
